fb77a48db4f84c6810f21e845e5f9dc8b6dff634
Daniel Navarro <danielkristopher0563@gmail.com>
2024-07-07 03:10:11 -0500

    ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,25 +65,25 @@ export default function Component() {
       </header>
       <main className="flex-1 pt-20">
         <section id="home" className="w-full py-12 md:py-24 lg:py-32">
-          <div className="container grid items-center gap-4 px-4 md:px-6 lg:grid-cols-2 lg:gap-6">
+          <div className="container mx-auto grid items-center gap-4 px-4 md:px-6 lg:grid-cols-2 lg:gap-6">
             <div className="space-y-4">
               <div className="space-y-2 text-center lg:text-left">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
                   Daniel Navarro
                 </h1>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Desarrollador Full Stak y diseñador gráfico con experiencia en
-                  crear soluciones digitales atractivas y funcionales.
+                  Desarrollador Full Stack y diseñador gráfico con experiencia
+                  en crear soluciones digitales atractivas y funcionales.
                 </p>
               </div>
             </div>
-            <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
+            <div className="flex flex-col gap-2 min-[400px] lg:flex-row lg:justify-end">
               <img
                 src="/Perfil.png"
                 width="400"
                 height="400"
                 alt="Profile"
-                className="mx-auto rounded-full w-40 h-40 object-cover"
+                className="mx-auto lg:ml-auto rounded-full w-40 h-40 object-cover"
               />
             </div>
           </div>
@@ -226,7 +226,7 @@ export default function Component() {
           id="about"
           className="w-full py-12 md:py-24 lg:py-32 bg-background/80"
         >
-          <div className="container grid items-center gap-4 px-4 md:px-6 lg:grid-cols-2 lg:gap-6">
+          <div className="container mx-auto  grid items-center gap-4 px-4 md:px-6 lg:grid-cols-2 lg:gap-6">
             <div className="space-y-4">
               <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">
                 Sobre mí
@@ -370,7 +370,7 @@ export default function Component() {
           id="contact"
           className="w-full py-12 md:py-24 lg:py-32  bg-muted"
         >
-          <div className="container grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
+          <div className="container mx-auto grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
             <div className="space-y-4">
               <div className="inline-block rounded-lg bg-muted px-3 py-1 text-sm">
                 Contacto
