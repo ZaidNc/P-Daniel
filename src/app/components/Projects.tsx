97fe3718b0065f161e61aae02b3fd2eb4278fff5
Daniel Navarro <danielkristopher0563@gmail.com>
2024-07-29 20:50:18 -0500

    ajuste margenes

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -72,7 +72,7 @@ export default function Projects() {
   const selectedProject = proyectos[selectedProjectIndex];
 
   return (
-    <div className="relative min-h-screen w-full flex flex-col items-center justify-center bg-background p-4">
+    <div className="relative min-h-screen w-full flex flex-col items-center bg-background p-4">
       <div className="w-full bg-muted">
         <div className="container mx-auto px-4 py-8 text-center">
           <h2 className="text-3xl font-heading font-bold tracking-tighter sm:text-4xl md:text-5xl">
@@ -85,7 +85,85 @@ export default function Projects() {
         </div>
       </div>
 
-      <div className="absolute top-1/2 left-0 transform -translate-y-1/2">
+      <div className="flex flex-grow items-center justify-center pt-8">
+        <div className="relative w-full max-w-xl p-6 bg-card bg-opacity-90 rounded-lg shadow-lg border border-border transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
+          <div className="text-center">
+            <h1 className="text-2xl font-heading font-bold mb-2">
+              {selectedProject.nombre}
+            </h1>
+            <p className="text-muted-foreground mb-4">
+              {selectedProject.descripcion}
+            </p>
+            <div className="mb-4">
+              {selectedProject.imagenes.map((imagen, index) => (
+                <div
+                  key={index}
+                  className="relative w-full h-64 overflow-hidden rounded-md"
+                >
+                  <CldImage
+                    src={imagen}
+                    width="800"
+                    height="600"
+                    crop="fill"
+                    quality="auto"
+                    alt={`Imagen ${index + 1}`}
+                    className="object-cover w-full h-full"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="space-y-4">
+              <div>
+                <h3 className="text-lg font-heading font-medium mb-2">
+                  Tecnologías Utilizadas
+                </h3>
+                <div className="flex flex-wrap gap-2 mt-2 justify-center">
+                  {selectedProject.tecnologias.map((tecnologia, index) => (
+                    <div
+                      key={index}
+                      className="bg-primary text-primary-foreground px-2 py-1 rounded-full text-xs transition-colors duration-300 hover:bg-secondary hover:text-secondary-foreground"
+                    >
+                      {tecnologia}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="flex items-center justify-center gap-4">
+                <Link
+                  href={selectedProject.despliegueUrl}
+                  passHref
+                  legacyBehavior
+                >
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center px-4 py-2 text-primary-foreground bg-primary border border-transparent rounded-full hover:bg-secondary hover:text-secondary-foreground transition-colors duration-300"
+                  >
+                    <ChevronRightIcon className="w-4 h-4 mr-2" />
+                    Ver Despliegue
+                  </a>
+                </Link>
+                <Link
+                  href={selectedProject.repositorioUrl}
+                  passHref
+                  legacyBehavior
+                >
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center px-4 py-2 text-primary-foreground bg-primary border border-transparent rounded-full hover:bg-secondary hover:text-secondary-foreground transition-colors duration-300"
+                  >
+                    <ChevronRightIcon className="w-4 h-4 mr-2" />
+                    Ver Repositorio
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 px-4">
         <button
           onClick={handlePrevProject}
           className="p-3 bg-primary text-primary-foreground rounded-full hover:bg-accent hover:text-accent-foreground transition-colors duration-300"
@@ -93,89 +171,13 @@ export default function Projects() {
           <ChevronLeftIcon className="w-8 h-8" />
         </button>
       </div>
-      <div className="absolute top-1/2 right-0 transform -translate-y-1/2">
+      <div className="absolute top-1/2 right-0 transform -translate-y-1/2 px-4">
         <button
           onClick={handleNextProject}
           className="p-3 bg-primary text-primary-foreground rounded-full hover:bg-accent hover:text-accent-foreground transition-colors duration-300"
         >
           <ChevronRightIcon className="w-8 h-8" />
         </button>
-      </div>
-
-      <div className="relative w-full max-w-xl p-6 bg-card bg-opacity-90 rounded-lg shadow-lg border border-border transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
-        <div className="text-center">
-          <h1 className="text-2xl font-heading font-bold mb-2">
-            {selectedProject.nombre}
-          </h1>
-          <p className="text-muted-foreground mb-4">
-            {selectedProject.descripcion}
-          </p>
-          <div className="mb-4">
-            {selectedProject.imagenes.map((imagen, index) => (
-              <div
-                key={index}
-                className="relative w-full h-64 overflow-hidden rounded-md"
-              >
-                <CldImage
-                  src={imagen}
-                  width="800"
-                  height="600"
-                  crop="fill"
-                  quality="auto"
-                  alt={`Imagen ${index + 1}`}
-                  className="object-cover w-full h-full"
-                />
-              </div>
-            ))}
-          </div>
-          <div className="space-y-4">
-            <div>
-              <h3 className="text-lg font-heading font-medium mb-2">
-                Tecnologías Utilizadas
-              </h3>
-              <div className="flex flex-wrap gap-2 mt-2 justify-center">
-                {selectedProject.tecnologias.map((tecnologia, index) => (
-                  <div
-                    key={index}
-                    className="bg-primary text-primary-foreground px-2 py-1 rounded-full text-xs transition-colors duration-300 hover:bg-secondary hover:text-secondary-foreground"
-                  >
-                    {tecnologia}
-                  </div>
-                ))}
-              </div>
-            </div>
-            <div className="flex items-center justify-center gap-4">
-              <Link
-                href={selectedProject.despliegueUrl}
-                passHref
-                legacyBehavior
-              >
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex items-center px-4 py-2 text-primary-foreground bg-primary border border-transparent rounded-full hover:bg-secondary hover:text-secondary-foreground transition-colors duration-300"
-                >
-                  <ChevronRightIcon className="w-4 h-4 mr-2" />
-                  Ver Despliegue
-                </a>
-              </Link>
-              <Link
-                href={selectedProject.repositorioUrl}
-                passHref
-                legacyBehavior
-              >
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="inline-flex items-center px-4 py-2 text-primary-foreground bg-primary border border-transparent rounded-full hover:bg-secondary hover:text-secondary-foreground transition-colors duration-300"
-                >
-                  <ChevronRightIcon className="w-4 h-4 mr-2" />
-                  Ver Repositorio
-                </a>
-              </Link>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
